4f4530dcdf8d186933845ceca9be7a773f34eb90
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-03 10:53:07 +0330

    Change left location of circle animate pulse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="absolute top-0 right-0 w-full h-full">
         <div className="absolute top-[10%] left-[15%] w-72 h-72 rounded-full bg-radial from-yellow-400/40 to-transparent blur-3xl animate-pulse-slow"></div>
         <div className="absolute bottom-[20%] right-[15%] w-84 h-84 rounded-full bg-radial from-yellow-400/40 to-transparent blur-3xl animate-pulse-slow animation-3000"></div>
-        <div className="absolute top-[45%] left-[50%] w-82 h-82 rounded-full bg-radial from-yellow-400/40 to-transparent blur-3xl animate-pulse-slow animation-2000"></div>
+        <div className="absolute top-[45%] left-[40%] w-82 h-82 rounded-full bg-radial from-yellow-400/40 to-transparent blur-3xl animate-pulse-slow animation-2000"></div>
       </div>
       <SignInSignUp />
     </div>
